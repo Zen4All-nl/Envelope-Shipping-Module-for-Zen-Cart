8dd3bb7f57c27ec8f6c7734fb8b1e14c62caa0cf
Zen4All <info@zen4all.nl>
2019-05-01 17:11:47 +0200

    updates

--- includes/modules/shipping/envelop.php
+++ includes/modules/shipping/envelop.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * @package shippingMethod
- * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Copyright 2003-2019 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: envelop.php 4184 2006-08-21 03:36:36Z ajeh $
+ * @version $Id: envelop.php$
  */
 /**
  * Enter description here...
@@ -14,31 +14,31 @@ class envelop extends base {
   /**
    * Enter description here...
    *
-   * @var unknown_type
+   * @var string
    */
   var $code;
   /**
    * Enter description here...
    *
-   * @var unknown_type
+   * @var string
    */
   var $title;
   /**
    * Enter description here...
    *
-   * @var unknown_type
+   * @var string
    */
   var $description;
   /**
    * Enter description here...
    *
-   * @var unknown_type
+   * @var string
    */
   var $icon;
   /**
    * Enter description here...
    *
-   * @var unknown_type
+   * @var boolean
    */
   var $enabled;
   /**
@@ -46,15 +46,16 @@ class envelop extends base {
    *
    * @return envelop
    */
-  function envelop() {
+  function __construct() {
     global $order, $db;
     //global $shipping_weight;
     $this->code = 'envelop';
     $this->title = MODULE_SHIPPING_ENVELOP_TEXT_TITLE;
     $this->description = MODULE_SHIPPING_ENVELOP_TEXT_DESCRIPTION;
-    $this->sort_order = MODULE_SHIPPING_ENVELOP_SORT_ORDER;
-    //$this->icon = '';
-    $this->icon = DIR_WS_MODULES . 'shipping/icons/tnt_logo.png';
+    $this->sort_order = defined('MODULE_SHIPPING_ENVELOP_SORT_ORDER') ? MODULE_SHIPPING_ENVELOP_SORT_ORDER : null;
+    if (null === $this->sort_order) return false;
+
+    $this->icon = '';
     $this->tax_class = MODULE_SHIPPING_ENVELOP_TAX_CLASS;
     $this->tax_basis = MODULE_SHIPPING_ENVELOP_TAX_BASIS;
     // disable only when entire cart is free shipping
@@ -64,7 +65,10 @@ class envelop extends base {
 
     if ( ($this->enabled == true) && ((int)MODULE_SHIPPING_ENVELOP_ZONE > 0) ) {
       $check_flag = false;
-      $check = $db->Execute("select zone_id from " . TABLE_ZONES_TO_GEO_ZONES . " where geo_zone_id = '" . MODULE_SHIPPING_ENVELOP_ZONE . "' and zone_country_id = '" . $order->delivery['country']['id'] . "' order by zone_id");
+      $check = $db->Execute("select zone_id from " . TABLE_ZONES_TO_GEO_ZONES . "
+                             where geo_zone_id = '" . MODULE_SHIPPING_ENVELOP_ZONE . "'
+                             and zone_country_id = " . (int)$order->delivery['country']['id'] . "
+                             order by zone_id");
       while (!$check->EOF) {
         if ($check->fields['zone_id'] < 1) {
           $check_flag = true;
@@ -80,25 +84,25 @@ class envelop extends base {
         $this->enabled = false;
       }
     }
-    
+
     //MODULE_SHIPPING_ENVELOP_UPPER_WEIGHT_LIMIT
     if($_SESSION['cart']->weight > MODULE_SHIPPING_ENVELOP_UPPER_WEIGHT_LIMIT){
-        $this->enabled = false;        
+        $this->enabled = false;
     }
-    
+
     if(!($this->cart_fits_envelope())){
-        $this->enabled = false;      
+        $this->enabled = false;
     }
 
   }
-  
+
   /**
    * Disables this module if the total volume of items in the cart exceeds envelope volume
    *
    * @param unknown_type $method
    * @return boolean
-   */  
-  
+   */
+
   function cart_fits_envelope(){
     global $db;
     // products_qty_envelope = VARCHAR allows it to be easily updated to '' (for MODULE_SHIPPING_ENVELOP_ENABLE_BY_DEFAULT)
@@ -111,44 +115,44 @@ class envelop extends base {
     $volume = 0;
 
     if(!(count($contents) > 0)) return false;
-    
+
     foreach($contents as $products_id => $value){
       // get number that fits envelope for this product from db
       if(($value['qty'] > 0)&&($products_id > 0)){
         $result = $db->Execute("select products_qty_envelope from " . TABLE_PRODUCTS . " where products_id = '" . $products_id . "'");
-      
+
         $products_qty_envelope = $result->fields['products_qty_envelope'];
-        
+
         if((MODULE_SHIPPING_ENVELOP_ENABLE_BY_DEFAULT == 'False')&&((int)$products_qty_envelope == 0)){
           // we found a product that is not set or does not fit
           return false;
-        } 
-        
-        // add checks for virtual products etc?         
+        }
+
+        // add checks for virtual products etc?
         if(($products_qty_envelope === '0')){
           // we found a product that is set but does not fit, no need to query more
           return false;
         }
-                
+
         if($products_qty_envelope > 0){
-          $products_volume = $value['qty']/(int)$products_qty_envelope;          
+          $products_volume = $value['qty']/(int)$products_qty_envelope;
           $volume = $volume + $products_volume;
         }
-      }   
-      // no need to query more products if the volume already exceeds the max value 
+      }
+      // no need to query more products if the volume already exceeds the max value
       if($volume > 1) return false;
-          
+
     }
     //echo $volume; // debug
     if($volume <= 1){
       // the total volume fits envelope
-      return true;  
+      return true;
     }
     // ??
     return false;
-  }  
-  
-  
+  }
+
+
   /**
    * Enter description here...
    *
@@ -173,7 +177,7 @@ class envelop extends base {
         break;
     }
 
-    $envelop_cost = split("[:,]" , MODULE_SHIPPING_ENVELOP_COST);
+    $envelop_cost = preg_split("/[:,]/" , MODULE_SHIPPING_ENVELOP_COST);
     $size = sizeof($envelop_cost);
     for ($i=0, $n=$size; $i<$n; $i+=2) {
       if (round($order_total,9) <= $envelop_cost[$i]) {
@@ -237,16 +241,16 @@ class envelop extends base {
   function install() {
     global $db;
     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) VALUES ('Enable Table Method', 'MODULE_SHIPPING_ENVELOP_STATUS', 'False', 'Do you want to offer envelop rate shipping?', '6', '0', 'zen_cfg_select_option(array(\'True\', \'False\'), ', now())");
-    
+
     //MODULE_SHIPPING_ENVELOP_WEIGHT
-    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Envelope weight', 'MODULE_SHIPPING_ENVELOP_WEIGHT', '40', 'The weight of an envelope + invoice + label etc.', '6', '0', now())");    
-        
-    //MODULE_SHIPPING_ENVELOP_UPPER_WEIGHT_LIMIT   
-    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Upper weight limit', 'MODULE_SHIPPING_ENVELOP_UPPER_WEIGHT_LIMIT', '2500', 'The maximum total weight of products in the cart (module is disabled if the total weight exceeds this limit).', '6', '0', now())");    
-    
+    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Envelope weight', 'MODULE_SHIPPING_ENVELOP_WEIGHT', '40', 'The weight of an envelope + invoice + label etc.', '6', '0', now())");
+
+    //MODULE_SHIPPING_ENVELOP_UPPER_WEIGHT_LIMIT
+    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Upper weight limit', 'MODULE_SHIPPING_ENVELOP_UPPER_WEIGHT_LIMIT', '2500', 'The maximum total weight of products in the cart (module is disabled if the total weight exceeds this limit).', '6', '0', now())");
+
     //define('MODULE_SHIPPING_ENVELOP_ENABLE_BY_DEFAULT','False');
     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) VALUES ('Enable by default.', 'MODULE_SHIPPING_ENVELOP_ENABLE_BY_DEFAULT', 'False', 'When set to True this module will stay enabled even if products_qty_envelope of (some of) the products in the cart is not set.', '6', '0', 'zen_cfg_select_option(array(\'True\', \'False\'), ', now())");
-   
+
     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Shipping Table', 'MODULE_SHIPPING_ENVELOP_COST', '100:2.1008,250:3.319,500:3.5714,3000:4.6218', 'The shipping cost is based on the total cost or weight of items or count of the items. Example: 25:8.50,50:5.50,etc.. Up to 25 charge 8.50, from there to 50 charge 5.50, etc', '6', '0', 'zen_cfg_textarea(', now())");
     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Table Method', 'MODULE_SHIPPING_ENVELOP_MODE', 'weight', 'The shipping cost is based on the order total or the total weight of the items ordered or the total number of items orderd.', '6', '0', 'zen_cfg_select_option(array(\'weight\', \'price\', \'item\'), ', now())");
     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Handling Fee', 'MODULE_SHIPPING_ENVELOP_HANDLING', '0', 'Handling fee for this shipping method.', '6', '0', now())");
@@ -274,4 +278,3 @@ class envelop extends base {
     return array('MODULE_SHIPPING_ENVELOP_STATUS', 'MODULE_SHIPPING_ENVELOP_ENABLE_BY_DEFAULT', 'MODULE_SHIPPING_ENVELOP_WEIGHT', 'MODULE_SHIPPING_ENVELOP_UPPER_WEIGHT_LIMIT', 'MODULE_SHIPPING_ENVELOP_COST', 'MODULE_SHIPPING_ENVELOP_MODE', 'MODULE_SHIPPING_ENVELOP_HANDLING', 'MODULE_SHIPPING_ENVELOP_TAX_CLASS', 'MODULE_SHIPPING_ENVELOP_TAX_BASIS', 'MODULE_SHIPPING_ENVELOP_ZONE', 'MODULE_SHIPPING_ENVELOP_SORT_ORDER');
   }
 }
-?>
