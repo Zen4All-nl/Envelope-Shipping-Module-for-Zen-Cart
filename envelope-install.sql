0f0792ab0928cb293df9322f41e13417db23c7a8
Zen4All <info@zen4all.nl>
2019-09-20 09:42:09 +0200

    remove qoutes

--- envelope-install.sql
+++ envelope-install.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `products` ADD `products_qty_envelope` VARCHAR( 32 ) NULL AFTER `products_weight` ;
+ALTER TABLE products ADD products_qty_envelope VARCHAR( 32 ) NULL AFTER products_weight;
